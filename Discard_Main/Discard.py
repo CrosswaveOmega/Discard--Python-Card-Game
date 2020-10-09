fc39057ed7584db8141ea5abc6ca6ef87c0e5395
CrosswaveOmega <xtream2pro@gmail.com>
2020-10-09 14:42:02 -0400

    TRYING TO REMOVE INFO. ver 2

--- Discard_Main/Discard.py
+++ Discard_Main/Discard.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from .main.grid import *
 from .main.piece import *
 
@@ -36,42 +35,3 @@ if __name__ == "__main__":
     duel.grid.print_grid()
     duel.grid.print_grid("south")
     duel.move_piece(testPiece)
-=======
-from .main.grid import *
-from .main.piece import *
-
-#The main game.
-
-class Card_Duel:
-    """Where the game will be played."""
-    def __init__(self):
-        self.game_id=0
-        self.val=0
-        self.players=[]
-        self.entity_list=[]
-        self.grid=Grid(5,5, self)
-    def add_piece(self, piece):
-        self.entity_list.append(piece)
-    def move_piece(self, piece):
-        piece.get_move_options(self.grid)
-
-
-#Driver Code.
-if __name__ == "__main__":
-    duel=Card_Duel()
-
-    testPiece=Piece("LO", "MY_NAME", 5,5, "MOVE?", "B1")
-    testPiece2=Piece("LOE", "OTHER", 5,5, "MOVE?", "D1")
-    testPiece3=Piece("LOE", "OTHER5", 5,5, "MOVE?", "A2")
-    duel.add_piece(testPiece)
-    duel.add_piece(testPiece2)
-    duel.add_piece(testPiece3)
-    print(testPiece.position.x_y())
-    duel.grid.print_grid()
-    duel.grid.print_grid("south")
-    duel.move_piece(testPiece)
-    testPiece.change_position("C2")
-    duel.grid.print_grid()
-    duel.grid.print_grid("south")
-    duel.move_piece(testPiece)
->>>>>>> 9633e9d3b26884942d418e615dc764ee5884ec0a
--- Discard_Main/classes/Cards/card.py
+++ Discard_Main/classes/Cards/card.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import discord
 import operator
 
@@ -61,67 +60,3 @@ class CardRetrievalClass():  #by no means what the final version should use.
 #Driver Code.
 if __name__ == "__main__":
     print(str(CardRetrievalClass().getByID(0xFFFFFFFF)))
-=======
-import discord
-import operator
-
-from discord.ext import commands, tasks
-from discord.utils import find
-from discord import Webhook, AsyncWebhookAdapter
-
-
-
-
-
-
-class CardBase():  #Wip.
-
-    def __init__(self, ID, name, icon, type, image="None"):
-        self.ID=ID          #• ID- The internal ID of the card.  All cards have this unique ID, consisting of a eight digit hexadecimal number
-                            #    Cards should be referenced by this ID, not their name.
-                            #    (00000000  to FFFFFFFF, makes maximum of 4,294,967,296 cards.
-        self.name=name      #• Name- The name of the card.  Customizable by a user,
-        self.icon=icon      #• Icon- A emoji that will represent this card, placed before.   Customizable by a user,<:thonkang:219069250692841473>
-        self.image=image    #• Image- Background image the card displays.   Customizable by a user,
-        self.type=type      #• Type- The type of card this is.
-
-    def __str__(self):
-        return self.icon + "|" + self.name + "|" + self.type + "|"+ str(self.ID)
-
-    def apply_custom(self, custom):
-        """Change the set values found in custom."""
-        if hasattr(custom, 'name'):
-            self.name=custom.name
-        if hasattr(custom, 'icon'):
-            self.icon=custom.icon
-        if hasattr(custom, 'image'):
-            self.icon=custom.icon
-        if hasattr(custom, 'type'):
-            self.type=custom.type
-
-
-
-class TestCard(CardBase):
-    """docstring for TestCard."""
-
-    def __init__(self, arg=0):
-        ID = 0xFFFFFFFF
-        name="Test Name"
-        icon="<:thonkang:219069250692841473>"
-        image="NA"
-        type="None"
-        super().__init__(ID,name, icon, type, image)
-        self.arg = arg
-
-
-class CardRetrievalClass():  #by no means what the final version should use.
-    def getByID(self, ID):
-        if(ID==0xFFFFFFFF):
-            return TestCard();
-
-
-
-#Driver Code.
-if __name__ == "__main__":
-    print(str(CardRetrievalClass().getByID(0xFFFFFFFF)))
->>>>>>> 9633e9d3b26884942d418e615dc764ee5884ec0a
