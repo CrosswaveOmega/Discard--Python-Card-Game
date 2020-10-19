37245dd1bba078a00ab0b7bf25792f0da0ef51ef
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2020-10-19 13:41:42 -0400

    beginning of userprofile

--- Discard_Main/classes/userservices/userprofile.py
+++ Discard_Main/classes/userservices/userprofile.py
@@ -6,16 +6,26 @@ from discord.utils import find
 from discord import Webhook, AsyncWebhookAdapter
 from pathlib import Path
 import pickle
-
-class SingleDictionary:
-    class __SingleDictionary: #Singleton Design.  Based on https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
+directory="saveData"
+class SingleUserProfile:
+    class __SingleUserProfile: #Singleton Design.  Based on https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
         def __init__(self, arg="P."):
             self.val = arg
             self.buffer={} #A buffer of all.
         def setAll(self):
             #Update for all new cardclusters.
             print("tbd")
-
+        def getIDinternal(self, id):
+            if(self.checkforidinbuffer(id)):
+                return self.buffer[str(id)]
+            else: #ID is not in buffer.
+                file=self.get_file_from_directory(id)
+                if(file!= None):
+                    print("Should initalize new user profile with the dictonary_to_use param set to the translated contents of file, add that into buffer under user id.")
+                    #Load contents of file into new UserProfile, add that into buffer.
+                else:
+                    print("Should initalize new default user profile, add that into buffer under user id.")
+                    #Initalize new file.
         def checkforidinbuffer(self, id):
             key_to_lookup = id
             if key_to_lookup in self.internal_Dictionary:
@@ -25,16 +35,25 @@ class SingleDictionary:
         def get_file_from_directory(self, id):
             filename="userprofile_id_"+str(id)+".json" #filename from json.
             file= Path(directory + "/"+ filename)
-            if file.exists():
+            if file.exists():#check if this file exists.
                 return file
+            else:
+                return None
+        def get_file_from_directory(self, id):
+            filename="userprofile_id_"+str(id)+".json" #filename from json.
+            file= Path(directory + "/"+ filename)
+            if file.exists():#check if this file exists.
+                return file
+            else:
+                return None
         def __str__(self):
             return repr(self) + self.val
     instance = None
     def __init__(self, arg):  #Internally, it keeps a single instance of the __SingleDictionary class in memory.
-        if not SingleDictionary.instance:
-            SingleDictionary.instance = SingleDictionary.__SingleDictionary(arg)
+        if not SingleUserProfile.instance:
+            SingleUserProfile.instance = SingleUserProfile.__SingleUserProfile(arg)
         else:
-            SingleDictionary.instance.val = arg
+            SingleUserProfile.instance.val = arg
     def getByID(self, id):
         return  #Average case of python dictionary lookup is O(1), worst case is O(n)
     def __getattr__(self, name):
@@ -42,7 +61,8 @@ class SingleDictionary:
 
 
 class UserProfile:
-    def __init__(self, dictonary_to_use=None):
+    def __init__(self, user_id, dictonary_to_use=None):
+        self.user_id=user_id
         self.cards=[] #List of ids.
         self.customs=[] #list of customs.
         self.exp=0
