c74a7e15f63193f18ddd8c184889bc59d28fd985
CrosswaveOmega <xtream2pro@gmail.com>
2020-10-20 13:34:38 -0400

    adding to userprofile

--- Discard_Main/classes/userservices/userprofile.py
+++ Discard_Main/classes/userservices/userprofile.py
@@ -5,7 +5,7 @@ from discord.ext import commands, tasks
 from discord.utils import find
 from discord import Webhook, AsyncWebhookAdapter
 from pathlib import Path
-import pickle
+import json
 directory="saveData"
 class SingleUserProfile:
     class __SingleUserProfile: #Singleton Design.  Based on https://python-3-patterns-idioms-test.readthedocs.io/en/latest/Singleton.html
@@ -21,13 +21,31 @@ class SingleUserProfile:
             else: #ID is not in buffer.
                 file=self.get_file_from_directory(id)
                 if(file!= None):
+                    f= file.open(mode='r');
+                    string=f.read();
+                    self.buffer[str(id)]=UserProfile(user_id==id, dictonary_to_use=json.loads(string))
+                    f.close()
                     print("Should initalize new user profile with the dictonary_to_use param set to the translated contents of file, add that into buffer under user id.")
                     #Load contents of file into new UserProfile, add that into buffer.
                 else:
+                    self.buffer[str(id)]=UserProfile(user_id==id)
                     print("Should initalize new default user profile, add that into buffer under user id.")
                     #Initalize new file.
+        def save_buffer_entry(self, id): #Saves the UserProfile object at id to a file.
+            if(self.checkforidinbuffer(id)):
+                filename="userprofile_id_"+str(id)+".json" #filename from json.
+                file= Path(directory + "/"+ filename)
+                f=file.open(mode="w+")
+
+                string_to_write=json.dumps(self.buffer[str(id)].to_dictionary(), sort_keys=True, indent=4)
+                f.write(string_to_write(string_to_write))
+                f.close()
+        def save_all_internal(self): #save everything in buffer.
+            for i, v in self.buffer.items():
+                self.save_buffer_entry(i)
+
         def checkforidinbuffer(self, id):
-            key_to_lookup = id
+            key_to_lookup = str(id)
             if key_to_lookup in self.internal_Dictionary:
               return True
             else:
@@ -48,7 +66,10 @@ class SingleUserProfile:
         else:
             SingleUserProfile.instance.val = arg
     def getByID(self, id):
-        return  #Average case of python dictionary lookup is O(1), worst case is O(n)
+        #Initial chekc
+        return SingleUserProfile.instance.getIDinternal(id)
+    def saveAll(self, id):
+
     def __getattr__(self, name):
         return getattr(self.instance, name)
 
@@ -58,7 +79,7 @@ class UserProfile:
         self.user_id=user_id
         self.cards=[] #List of ids. every entry in list is dictionary of format {"card_id":card_id, "custom":id_of_custom_applied}
         self.customs=[] #list of customs made by the user.
-        self.cards_customs={} #dictionary.  Key is card Id and
+        #self.cards_customs={} #dictionary.  Key is card Id and
         self.exp=0
         self.level=0
         self.cardcount=0
@@ -66,7 +87,7 @@ class UserProfile:
         self.stars=0
 
         self.decks=[] #Max 99.
-        
+
         if(dictonary_to_use!=None):
             for i, v in dictonary_to_use.items():
                 if hasattr(self, i):
@@ -83,6 +104,7 @@ class UserProfile:
 
     def add_card(self, card_id):
         self.cards.append({"card_id":card_id, "custom":None})
+
     def get_exp(self):
         return self.exp
     def set_exp(self, value):
