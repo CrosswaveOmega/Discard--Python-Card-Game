d2f183b13d48353e5a30372cd60f081957bbd0cb
CrosswaveOmega <xtream2pro@gmail.com>
2020-10-22 13:51:38 -0400

    fixed a typo in the example creaturecard.

--- Discard_Main/classes/Cards/CardLibrary/cardcluster1.py
+++ Discard_Main/classes/Cards/CardLibrary/cardcluster1.py
@@ -60,21 +60,22 @@ class TestCreature1(card.CreatureCard):
         hp=10
         speed=9 #Speed is 0-99.
         #Defining summon cost.
-        summoncost_r=3
-        summoncost_b=2
-        summoncost_g=2
+        summonr=3
+        summonb=2
+        summong=2
         #Defining Skills.
         skill1=CustomSkill1("Custom Name 1", "trigger", "target", "type", "limit", "Type description here.")
         skill2=CustomSkill2("Custom Name 2", "trigger", "target", "type", "limit", "Type description here.")
         skill3=CustomSkill3("Custom Name 2", "trigger", "target", "type", "limit", "Type description here.")
         #Defining move Style.
         movestyle="STEP 2"
-        movelimit=9
+        movelimit=1
 
         super().__init__(self.ID, name, icon, image=image, \
-        hp=hp, speed=2, summoncost_r=0, summoncost_b=0, summoncost_g=0, \
-        skill_1=None, skill_2=None, skill_3=None, \
-        movestyle="", movelimit=1)
+        hp=hp, speed=2,\
+        summoncost_r=summonr, summoncost_b=summonb, summoncost_g=summong, \
+        skill_1=skill1, skill_2=skill2, skill_3=skill3, \
+        movestyle=movestyle, movelimit=movelimit)
     class CustomSkill1(card.Skill):#Nested class
         def __init__(self, name="No name set.", trigger="command", target="other", type="tbd", limit="tbd", description="none"): #there's probably a better way to do this.
             super().__init__(name, trigger, target, type, limit, description)
