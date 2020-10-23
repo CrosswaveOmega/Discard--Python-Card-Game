c090a12e48381a3d46f6eec086931e67b9d5a698
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2020-10-23 15:04:49 -0400

    TargetingStuff
    Made a target_maker module.  It takes a series of arguments, and puts them into a dictionary to be used for a target.
    
    Arguments do not need to be in any order.

---
+++ Discard_Main/classes/main/target_maker.py
@@ -0,0 +1,55 @@
+from .generic.notationhelp import space_notation_to_value, to_notation, get_letter
+from .generic.position import Position
+
+
+
+{
+"shape": "Type of Range",
+"dist":"The Number of Spaces covered.",
+"scope": "The Scope of the target.",
+"amount":"The amount of entities within target.",
+"limit": ["Additional Conditions"]
+}
+optionsDictionary={
+"shape": ["Cardinal", "Diagonal", "Omni", "Adjacent", "Rectilinear", "Row", "Column", "Any", "Other"],
+"dist": "", #jUST A NUMBER
+"scope": ["Enemy", "Ally","Both","Self","Else"],
+"amount":["Single","x1","x2","x3","x4","x5","x6","x7","x8","x9","All"],
+"limit": ["None"]
+}
+
+def Args_To_Target(args*):
+    new_dictionary={
+    "shape":"", #required
+    "dist": 1 ,
+    "scope":"", #required
+    "amount":"", #required
+    "limit": [], #optional
+    }
+    number_of_args=len(args) #min is 3,
+    #match based on universal list.
+    shapematch=False
+    distmatch=False
+    scopematch=False
+    amountmatch=False
+    for element in args:
+        if not shapematch:
+            if(element in optionsDictionary["shape"]):
+                new_dictionary["shape"]=element
+                shapematch=True
+        if not scopematch:
+            if(element in optionsDictionary["scope"]):
+                new_dictionary["scope"]=element
+                scopematch=True
+        if not amountmatch:
+            if(element in optionsDictionary["amount"]):
+                new_dictionary["amount"]=element
+                amountmatch=True
+        if not distmatch:
+            stringvertotest=string(element)
+            if(stringvertotest.isnumeric()):
+                new_dictionary["dist"]=int(stringvertotest)
+                distmatch=True
+        if(element in optionsDictionary["limit"]):
+            new_dictionary["limit"].append(element)
+    return new_dictionary
