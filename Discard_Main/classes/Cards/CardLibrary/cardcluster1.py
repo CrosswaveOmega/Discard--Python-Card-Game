6958e5b800a999234b8cfe90ca56e6f9d93d04ec
letosky <stephenletosky@gmail.com>
2020-10-29 12:25:59 -0400

    Added some test creatures which use some of the new skills available. These are rough in nature and still need some work.

--- Discard_Main/classes/Cards/CardLibrary/cardcluster1.py
+++ Discard_Main/classes/Cards/CardLibrary/cardcluster1.py
@@ -76,3 +76,165 @@ class TestCreature1(card.CreatureCard):
         summoncost_r=summonr, summoncost_b=summonb, summoncost_g=summong, \
         skill_1=skill1, skill_2=skill2, skill_3=skill3, \
         movestyle=movestyle, movelimit=movelimit)
+
+
+class TestCreatureAlpha(card.CreatureCard):
+    # This is test creature alpha, so its ID is A for now.
+    ID = 0x0000A
+
+    def __init__(self):
+        self.ID = 0x0000A
+        name = "Alpha"
+        icon = ""
+        image = ""
+        type = ""
+        hp = 5
+        speed = 20
+
+        # The summoning cost of this creature
+        summonr = 2
+        summonb = 2
+        summong = 2
+
+        #These are the creature's skills
+        skill1 = MultiAttack("Multi Attacks", "command", ("Rectilinear", "Enemy", "x3"), "attack", "limit", "description", damage=1, attacks=3, damage_tag="")
+        skill2 = BasicShield("Shield", "command", ("self", "Ally", "x1"), "limit", "description", shield_amount=2)
+        skill3 = BasicHeal("BasicHeal", "command", ("Any", "Ally", "x1"), "support", "limit", "description", heal_amount=1)
+
+        # This defines the creature's move style
+        move_style = "STEP 1"
+        movelimit = 1
+
+        super().__init__(self.ID, name, icon,image=image, hp=hp, speed=speed, summoncost_r=summonr, summoncost_b=summonb, summoncost_g=summong,
+                         skill_1=skill1, skill_2=skill2,skill_3=skill3, movestyle=move_style, movelimit=movelimit)
+
+
+class TestCreatureBeta(card.CreatureCard):
+    # This is test creature beta, so its ID is B for now.
+    ID = 0x0000B
+
+    def __init__(self):
+        self.ID = 0x0000B
+        name = "Beta"
+        icon = ""
+        image = ""
+        type = ""
+        hp = 15
+        speed = 4
+
+        # The summoning cost of this creature
+        summonr = 3
+        summonb = 3
+        summong = 3
+
+        # These are the creature's skills
+        skill1 = MultiAttack("Multi Attacks", "command", ("Rectilinear", "Enemy", "x3"), "attack", "limit",
+                             "description", damage=1, attacks=3, damage_tag="")
+        skill2 = BasicShield("Shield", "command", ("self", "Ally", "x1"), "limit", "description", shield_amount=2)
+        skill3 = BasicHeal("BasicHeal", "command", ("Any", "Ally", "x1"), "support", "limit", "description",
+                           heal_amount=1)
+
+        # This defines the creature's move style
+        move_style = "SAME COLUMN LIMIT 3"
+        movelimit = 1
+
+        super().__init__(self.ID, name, icon, image=image, hp=hp, speed=speed, summoncost_r=summonr,
+                         summoncost_b=summonb, summoncost_g=summong,
+                         skill_1=skill1, skill_2=skill2, skill_3=skill3, movestyle=move_style, movelimit=movelimit)
+
+
+class TestCreatureDelta(card.CreatureCard):
+    # This is test creature delta, so its ID is D for now.
+    ID = 0x0000D
+
+    def __init__(self):
+        self.ID = 0x0000D
+        name = "Delta"
+        icon = ""
+        image = ""
+        type = ""
+        hp = 9
+        speed = 8
+
+        # The summoning cost of this creature
+        summonr = 1
+        summonb = 2
+        summong = 1
+
+        # These are the creature's skills
+        skill1 = BasicAttack("Custom Name 1", "command", ("Adjacent", "Enemy", "x1"), "type", "limit", "Type description here.", damage=5)
+        skill2 = BasicShield("Shield", "command", ("self", "Ally", "x1"), "limit", "description", shield_amount=2)
+        skill3 = BasicHeal("BasicHeal", "command", ("Any", "Ally", "x1"), "support", "limit", "description",
+                           heal_amount=1)
+
+        # This defines the creature's move style
+        move_style = "SAME DIAGONAL LIMIT 2"
+        movelimit = 1
+
+        super().__init__(self.ID, name, icon, image=image, hp=hp, speed=speed, summoncost_r=summonr,
+                         summoncost_b=summonb, summoncost_g=summong,
+                         skill_1=skill1, skill_2=skill2, skill_3=skill3, movestyle=move_style, movelimit=movelimit)
+
+class TestCreature2(card.CreatureCard):
+    # This is test creature 2, so its ID is 2 for now.
+    ID = 0x00004
+
+    def __init__(self):
+        self.ID = 0x00004
+        name = "Creature2"
+        icon = ""
+        image = ""
+        type = ""
+        hp = 7
+        speed = 9
+
+        # The summoning cost of this creature
+        summonr = 2
+        summonb = 4
+        summong = 1
+
+        # These are the creature's skills
+        skill1 = BasicAttack("Custom Name 1", "command", ("Adjacent", "Enemy", "x1"), "type", "limit", "Type description here.", damage=6)
+        skill2 = BasicShield("Shield", "command", ("self", "Ally", "x1"), "limit", "description", shield_amount=1)
+        skill3 = BasicHeal("BasicHeal", "command", ("Any", "Ally", "x1"), "support", "limit", "description",
+                           heal_amount=2)
+
+        # This defines the creature's move style
+        move_style = "STEP 2"
+        movelimit = 1
+
+        super().__init__(self.ID, name, icon, image=image, hp=hp, speed=speed, summoncost_r=summonr,
+                         summoncost_b=summonb, summoncost_g=summong,
+                         skill_1=skill1, skill_2=skill2, skill_3=skill3, movestyle=move_style, movelimit=movelimit)
+
+class TestCreature3(card.CreatureCard):
+    # This is test creature 3, so its ID is 3 for now.
+    ID = 0x00003
+
+    def __init__(self):
+        self.ID = 0x00003
+        name = "creature 3"
+        icon = ""
+        image = ""
+        type = ""
+        hp = 9
+        speed = 8
+
+        # The summoning cost of this creature
+        summonr = 3
+        summonb = 2
+        summong = 3
+
+        # These are the creature's skills
+        skill1 = BasicAttack("Custom Name 1", "command", ("Adjacent", "Enemy", "x1"), "type", "limit", "Type description here.", damage=8)
+        skill2 = BasicShield("Shield", "command", ("self", "Ally", "x1"), "limit", "description", shield_amount=1)
+        skill3 = BasicHeal("BasicHeal", "command", ("Any", "Ally", "x1"), "support", "limit", "description",
+                           heal_amount=3)
+
+        # This defines the creature's move style
+        move_style = "HOP X -2 Y -2"
+        movelimit = 1
+
+        super().__init__(self.ID, name, icon, image=image, hp=hp, speed=speed, summoncost_r=summonr,
+                         summoncost_b=summonb, summoncost_g=summong,
+                         skill_1=skill1, skill_2=skill2, skill_3=skill3, movestyle=move_style, movelimit=movelimit)
