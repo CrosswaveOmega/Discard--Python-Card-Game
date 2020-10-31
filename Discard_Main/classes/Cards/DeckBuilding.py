f3ae16dc08e4d87d9d191e53e3cc7939aba00c25
dtran22 <72167289+dtran22@users.noreply.github.com>
2020-10-31 03:11:18 -0400

    Update DeckBuilding.py

--- Discard_Main/classes/Cards/DeckBuilding.py
+++ Discard_Main/classes/Cards/DeckBuilding.py
@@ -10,6 +10,7 @@ class Deck():
 
         self.deck_cards = []
 
+    #Setters and Getters
     def set_deck_name(self, deck_name):
         self.deck_name = deck_name
 
@@ -25,21 +26,38 @@ class Deck():
     def get_deck_cards(self):
         return self.deck_cards
 
-    def get_card(self, card_id):
-        return CardRetrievalClass().getByID(int(card_id, 16))
+    #def get_card(self, card_id):
+        #return CardRetrievalClass().getByID(int(card_id, 16))
+
+    def inDeck(self, card_id):
+        #Check if the card_id is in the current deck
+        for i in range(len(self.deck_cards)):
+            if (self.deck_cards[i]["card_id"] == card_id):
+                return True
+        return False
 
     def addToDeck(self, card_id):
+        #Add card_id along with its custom_id (if available) to the deck
         self.deck_cards.append({"card_id":card_id, "custom":None})
 
     def removeFromDeck(self, card_id):
+        #Searches the deck for the card_id for removal and removes it
         for i in range(len(self.deck_cards)):
-            if self.deck_cards[i]["card_id"] == card_id:
+            if (self.deck_cards[i]["card_id"] == card_id):
                 del self.deck_cards[i]
-                break
+                return
 
-    def addListToDeck(self, cards): #list of cards to add
+    def addListToDeck(self, cards):
+        #[cards] is a list of card_ids
+        #Add a list of cards to the deck
         for i in cards:
             self.deck_cards.append(i)
 
-    def removeListFromDeck(self, cards): #List of cards to remove
-        return 
+    def removeListFromDeck(self, cards):
+        #[cards] is a list of card_ids
+        #Searches the deck for all the cards corresponding to the list passed by the argument and remove them
+        counter = 0
+        for i in self.deck_cards:
+            if (self.deck_cards[counter]["card_id"] in cards):
+                del self.deck_cards[counter]["card_id"]
+            counter = counter + 1
