e6340b2f423b84d9abf2e27fb4efe76ad74b4ab0
CrosswaveOmega <62901101+CrosswaveOmega@users.noreply.github.com>
2020-11-17 14:39:26 -0500

    2 Player Duel Command

--- Discard_Main/Cards_Battle_Cog.py
+++ Discard_Main/Cards_Battle_Cog.py
@@ -34,6 +34,20 @@ from .Discard import *
 
     #<a:stopwatch:774741008495542284>
     #<a:stopwatch_15:774741008793337856>
+async def get_everything(bot, author, channel):
+    """Returns profile and player of the passed in info"""
+    print("get profile of user.")
+    profile = SingleUser.getById(author.id)
+    playerDeck=[]
+    print("Right now, it only gets the first deck of the user.")
+    playerDeck_RAW = profile.get_decks()[0] #inventory entries.
+    for deck_card in playerDeck_RAW.get_deck_cards():
+        newcard=await inventory_entry_to_card_object(bot, deck_card)
+        playerDeck.append(newcard)
+    player_DPIOS=DPIOS(channel, author, bot)
+    await player_DPIOS.send_order()
+    player = DiscordPlayer(deck=playerDeck, team=1, dpios=player_DPIOS)
+    return profile, player
 
 class CardCogBattle(commands.Cog):
     """Commands for battle system goes here."""
@@ -77,12 +91,61 @@ class CardCogBattle(commands.Cog):
         channel=ctx.message.channel;
 
         server=channel.guild
-
+``
         room1=server.fetch_channel(777997792558710804)
         room2=server.fetch_channel(777998040283086900)
 
+        user1=None
+        user2=None
+        def check_message_1(m):
+            return m.author != bot.user and m.channel == room1
+        def check_message_2(m):
+            return m.author != bot.user and m.channel == room2
 
-        channel_1=server.fetch
+        async def getMessageInChannel1():
+            msg=await bot.wait_for('message', check=check_message_1)
+            #print("Message ");
+            return msg.author
+        async def getMessageInChannel2():
+            msg=await bot.wait_for('message', check=check_message_2)
+            #print("Message ");
+            return msg.author
+
+        messagetask1 = asyncio.create_task(getMessageInChannel1())
+        messagetask2 = asyncio.create_task(getMessageInChannel2())
+        tasklist=[messagetask, messagetask2]
+
+        await channel.send("Please have one user say something in room1, and have another user say something in room 2.")
+        done, pending = await asyncio.wait(tasklist, return_when=asyncio.ALL_COMPLETED) #there's probably a better way to do this.
+        if messagetask1 in done:
+            user1=messagetask1.result();
+        if messagetask2 in done:
+            user2=messagetask2.result();
+
+        profile1, player1= await get_everything(bot, user1, room1)
+        profile2, player2= await get_everything(bot, user2, room2)
+
+        thisDuel=Card_Duel(bot)
+        thisDuel.addPlayer(player1)
+        thisDuel.addPlayer(player2)
+        #Start Card_Duel
+        testPiece=Leader(player1, "MY_NAME", position_notation="C1")
+        testPiece.set_image()
+
+        testPiece2=Leader(player2, "MY_NAME", position_notation="C5")
+        testPiece2.set_image()
+
+
+        player1.set_leader(testPiece)
+        player2.set_leader(testPiece2)
+
+
+
+        thisDuel.add_piece(testPiece)
+        thisDuel.add_piece(testPiece2)
+
+        await thisDuel.start_game()
+        
     @commands.command()
     async def start_test_duel (self, ctx, *args): #Start a duel
         '''
